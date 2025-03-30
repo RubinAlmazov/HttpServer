c7c64cd5d34a5ab618a451893bd6c200ecfb62dd
Rubin <lazarettorg@gmail.com>
2025-03-30 14:14:36 +0400

    add handlerequest method

--- src/NioHttpServer.java
+++ src/NioHttpServer.java
@@ -23,6 +23,7 @@ public class NioHttpServer {
             selector.select();
             for (SelectionKey key : selector.selectedKeys()) {
                 if (key.isAcceptable()) accept(selector, serverSocket);
+                if (key.isReadable()) handleRequest(key);
             }
             selector.selectedKeys().clear();
         }
@@ -35,7 +36,26 @@ public class NioHttpServer {
         System.out.println("New connection: " + client.getRemoteAddress());
     }
 
+    public static void handleRequest(SelectionKey key) throws IOException {
+        SocketChannel client = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int bytesRead = client.read(buffer);
 
+        if (bytesRead == -1) {
+            client.close();
+            return;
+        }
+
+        buffer.flip();
+
+        String requst = new String(buffer.array(), 0, bytesRead);
+        System.out.println("Request:\n " + requst);
+
+        String response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+        ByteBuffer responseBuffer = ByteBuffer.wrap(response.getBytes());
+        client.write(responseBuffer);
+        client.close();
+    }
 
 
 }
